8addbe39ab0bb1dc9c53ad0d0878ba42c3e260d0
Huxzhi <huxzhi@gmail.com>
2023-02-28 18:52:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+---
+date: 2023-02-28 18:25
+title: README
+---
+
+## 什么是 front matter
+front matter的中文意思是前言，几乎每本书都会有前言，用来说明写书目的或者内容总结。
+
+而 markdown 文件中的front matter指的是以yaml格式在文件开头增加的元数据，示例如下：
+
+```
+---
+title: "为 obsidian 中的文件批量添加 front matter"
+date: 2023-02-28 18:25
+tags:
+- obsidian
+- frontmatter
+---
+```
+front matter需要用---包裹
+
+笔记软件或博客软件会拿到这些元数据做相应处理，比如上面这个front matter中 title 字段用来作为文章题目，date作为发布日期，tags 作为文章的标签列表
+
+## 使用方法
+
+下载 `python-frontmatter`
+```sh
+pip3 install python-frontmatter
+```
+
+执行本脚本
+
+```sh
+python3 front-matter.py
+```
+
+## 效果
+
+自动添加 `标题` 和 `日期`
+
+```
+---
+date: 2023-02-28 18:25
+title: README
+---
+```
---
+++ front-matter.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+
+import os,platform
+import time
+
+import frontmatter
+
+def update_front_matter(file):
+
+    with open(file, 'r', encoding='utf-8') as f:
+       post = frontmatter.loads(f.read())
+    
+    is_write = False
+      
+    # 获取标题   
+    if not post.metadata.get('title', None):  
+        # 去掉 .md 后缀
+        title=file.split('/')[-1][:-3]
+        print('获取标题 '+title)
+        post['title'] =title
+        if not is_write:
+            is_write = True
+
+
+    if not post.metadata.get('date', None):
+        
+        # macOS
+        if platform.system() =='Darwin':
+            timeArray = time.localtime(os.stat(file).st_birthtime)
+        # Window下 os.path(file).getctime()
+        else:
+            timeArray = time.localtime(os.path(file).getctime())
+        createTime = time.strftime("%Y-%m-%d %H:%M", timeArray)
+        print('获取标题 '+createTime)
+        post['date'] = createTime
+        if not is_write:
+            is_write = True
+
+    if is_write:
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(frontmatter.dumps(post))
+  
+
+# 递归获取提供目录下所有文件
+def list_all_files(root_path, ignore_dirs=[]):
+    files = []
+    default_dirs = [".git", ".obsidian", ".config"]
+    ignore_dirs.extend(default_dirs)
+
+    for parent, dirs, filenames in os.walk(root_path):
+        dirs[:] = [d for d in dirs if not d in ignore_dirs]
+        filenames = [f for f in filenames if not f[0] == '.']
+        for file in filenames:
+            if file.endswith(".md"):
+                files.append(os.path.join(parent, file))
+    return files
+
+
+if __name__ == "__main__":
+    # 默认忽略文件
+    ignore_dirs = [".obsidian", "Write"]
+    files = list_all_files('./', ignore_dirs=ignore_dirs)
+
+    print("current dir: ", os.path.dirname(os.path.abspath(__file__)))
+    for file in files:
+        print(platform.system())    
+        print("---------------------------------------------------------------")
+        print('current file: ', file)
+        update_front_matter(file)
+        time.sleep(1)
